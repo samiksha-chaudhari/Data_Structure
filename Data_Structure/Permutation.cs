fc8a45d7b7541821417ae462e6a1b32fe50e04f4
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-25 18:22:03 +0530

    finding parmutation

---
+++ Data_Structure/Permutation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structure
+{
+    class Permutation
+    {
+        /// <summary>
+        /// method to calculate parmutation 
+        /// </summary>
+        /// <param name="str"> string provided for parmutation </param>
+        /// <param name="l"> starting index</param>
+        /// <param name="r"> end index </param>
+        public void permute(string str, int start, int end)
+        {
+            if (start == end)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    str = swap(str, start, i);
+                    permute(str, start + 1, end);
+                    str = swap(str, start, i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// method to swap character at position and return swapped string
+        /// </summary>
+        /// <param name="a"> string value </param>
+        /// <param name="i"> position 1 </param>
+        /// <param name="j"> position 2 </param>
+        /// <returns></returns>
+        public static string swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+    }
+        
+}
--- Data_Structure/Program.cs
+++ Data_Structure/Program.cs
@@ -7,6 +7,12 @@ namespace Data_Structure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to data structure");
+            Console.WriteLine("Enter string for permutation : ");
+            string str = Console.ReadLine();
+            Console.WriteLine("\n");
+            int n = str.Length;
+            Permutation par = new Permutation();
+            par.permute(str, 0, n - 1);
         }
     }
 }
