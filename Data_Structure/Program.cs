aa2e84274b88601bae7fdb3de0928fdf0469c566
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-25 17:56:31 +0530

    welcome

---
+++ Data_Structure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure");
+        }
+    }
+}
