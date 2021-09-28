6bd805397d9b22b87cb5066dce804b1aaa751b6f
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-28 17:38:09 +0530

    updating

--- Data_Structure/BubbleSort.cs
+++ Data_Structure/BubbleSort.cs
@@ -14,13 +14,13 @@ namespace Data_Structure
             {
                 for (int i = 0; i <= arr.Length - 2; i++)
                 {
-                    if (arr[i] > arr[i + 1])
+                    if (arr[i] > arr[i + 1]) //if num > num2 then swap
                     {
-                        temp = arr[i + 1];
+                        temp = arr[i + 1];// swap temp and arr[i]
                         arr[i + 1] = arr[i];
                         arr[i] = temp;
                         Console.WriteLine("Sorted:");
-                        foreach (int p in arr)
+                        foreach (int p in arr) //print array
                             Console.WriteLine(p + " ");
                     }
                 }
